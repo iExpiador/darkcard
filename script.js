e7cf5442ce6453f05a76a9a57e6c8955eae82628
PoppyTime <mirianeven25@gmail.com>
2025-06-03 20:20:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".btn.forum").addEventListener("click", () => {
-    window.open("https://www.youtube.com/watch?v=Pj5H4qP3jSk", "_blank");
+    window.open("https://www.youtube.com/watch?v=aIHF7u9Wwiw", "_blank");
   });
 
   document.querySelector(".btn.deseados").addEventListener("click", () => {
-    window.open("https://www.youtube.com/watch?v=C45hZtVt2Pc", "_blank");
+    window.open("https://www.youtube.com/watch?v=SsKT0s5J8ko", "_blank");
   });
 
   document.querySelector(".btn.blog").addEventListener("click", () => {
-    window.open("https://xat.com/", "_blank");
+    window.open("https://www.youtube.com/watch?v=NgsWGfUlwJI", "_blank");
   });
 
   console.log("Dark profile card loaded.");
